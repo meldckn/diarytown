4d59e2b3180fece44e72b04ba2dd21392da56342
Melanie Dickinson <meldckn@gmail.com>
2019-10-29 14:10:10 -0700

    Separated SortableJS code into its own module, removed RiTa

--- diary.js
+++ diary.js
@@ -65,11 +65,6 @@ function removeDuplicates (array) {
 
 DataWrangler.init();
 
-// Need to custom-add some words to the lexicon because the 
-// default Rita lexicon thinks they're nouns when they're actually verbs
-RiTa.addWord("meditated","m-eh1-d-ah0-t-ey2-t-eh-d","vbd");
-RiTa.addWord("gossiped", "g-aa1-s-ah0-p-eh-d", "vbd");
-
 // Custom plugin for NLP-compromise to force particular verb conjugations
 const plugin = {
 	conjugations: {
@@ -87,60 +82,20 @@ const plugin = {
 	    'reminisce': { Infinitive:'reminisce', PastTense:'reminisced', Gerund:'reminiscing'},
 	    'storm': 	 { Infinitive:'storm', PastTense:'stormed', Gerund:'storming'},
 	    'treat': 	 { Infinitive:'treat', PastTense:'treated'},
-	    'visit': 	 { Infinitive:'visit', PastTense:'visited'}
+	    'visit': 	 { Infinitive:'visit', PastTense:'visited'},
+	    'watch': 	 { Infinitive:'watch', PastTense:'watched', Gerund:'watching'},
+	    'people-watch':{Infinitive:'people-watch',PastTense:'people-watched', Gerund:'people-watching'}
 	}
 };
 nlp.plugin(plugin);
 
-/// Sortable library stuff
-
 makeLibrary();
+makeLibrarySortables();
 
 let diary = document.getElementById('diary');
 let library = document.querySelector('.library'); // Might eventually be multiple lists
 let trash = document.getElementById('trash');
 trash.draggable = false;
- 
-// TODO since this doesn't allow arbitrarily nested inner slots,
-// might try making different Sortable group for each inner slot (by incrementing an id)?
-// and then connecting all existing inner slot groups
-// --Would also need to rework CSS probably
-// TODO ** Instead of inner sortable slots (since they make for small targets)
-// make it so when you drag and drop something over a phrase with a slot, 
-// it gets added to that slot (by hand; don't make inner slots Sortable lists).
-// if there are multiple slots in phrase, check mouse position relative to boundingRect
-let innerSortableObject = {
-	group: {
-		name: "inner",
-		pull: ["trash", "diary", "inner"], // lists that can pull from any inner slot 
-		put: ["library", "diary", "inner"], // lists that can add to any inner slot
-		revertClone: true,
-	},
-	animation: 200,
-	//fallbackOnBody: true,
-	sort: true,
-	// px, distance mouse must be from empty sortable to insert drag element into 
-	emptyInsertThreshold: 20,
-	filter: ".empty-indicator",  // Selectors that do not lead to dragging
-	onAdd: function (evt) {
-		// When a phrase is added to an inner slot, hide its empty-indicator
-		// and show a distinguishing background color
-		// Use :scope selector to select only direct children, not descendants
-		evt.to.querySelector(':scope > .empty-indicator').style.display = "none";
-		// TODO slightly darken the inner slot's parent color for the inner slot background 
-		evt.to.style.background = "#00a6de"; 
-		transformCompoundPhrase(evt.to.parentElement.parentElement, evt.item);
-	},
-	// Element is removed from the list into another list
-	onRemove: function (evt) {
-		// if list is empty, show empty indicator symbol
-		// TODO try doing this onChange so it's updated during the drag?
-		if (evt.from.children.length <= 1) {
-			evt.from.querySelector(':scope > .empty-indicator').style.display = "inline-block";
-			evt.from.style.background = "inherit";
-		}
-	}
-};
 
 function transformCompoundPhrase (outerPhrase, newPhrase) {
 	let slot = getSlotNumber(newPhrase);
@@ -185,6 +140,11 @@ function transformCompoundPhrase (outerPhrase, newPhrase) {
 			addSubject(newPhrase);
 			toPast(newPhrase);
 			break;
+		case 'finished':
+		case 'started':
+			stripI(newPhrase);
+			toPresentParticiple(newPhrase);
+			break;
 		case 'which-caused':
 			//after event changes to present tense and prefix "#subject to" 
 			//(if action: change to present tense and prefix with "me to")
@@ -217,15 +177,20 @@ function stripI (phraseElement) {
 	phraseElement.querySelector('.phrase-container p').innerText = phraseText.replace("I ", "");
 }
 
-// Use RiTa to find the first verb in the given phrase (string, not element)
+// Use NLP Compromise to find the first verb in the given phrase (string, not element)
 // Since that's probably the main verb and the only thing we need to change the tense of
 function getVerb (phrase) {
-	let words = RiTa.tokenize(phrase);
-	let pos = RiTa.getPosTags(phrase, true);
-	let firstVerb = words.find( (word,i) => {
-		return pos[i] === "v";
-	});
-	return firstVerb;
+	let terms = nlp(phrase).terms().data();
+	let verb = terms.find( (term,i) => {
+		// Find the first term with "Verb" in its POS tags
+		// Unless it's hyphenated, then return the second (e.g., cloud-watched --> watched)
+		// Will break if there's a non-verb hyphenation before the first verb
+		if (term.tags.includes("Hyphenated")) 
+			return term.spaceAfter !== "-";
+		else
+			return term.tags.includes("Verb");
+	})
+	return verb.text;
 }
 
 // Return the given verb conjugated to the given tense
@@ -243,8 +208,12 @@ function conjugateVerb (verb, tense) {
 	}
 }
 
-// TODO write a test function that runs on load that tries to conjugate all phrases 
+// TODO Test function that runs on load that tries to conjugate all phrases 
 // in library, and lists all phrases that it doesn't understand
+function testAllConjugations () {
+	let phrases = DataWrangler.getAllPhrases();
+	/////
+}
 
 // Return an array of all unique single-word verbs used in the phrases library
 // Mostly for debugging
@@ -278,81 +247,16 @@ function conjugatePhraseElement(phraseElement, tense) {
 
 	let phraseText = phraseElement.querySelector('.phrase-container p').innerText;
 	let verb = getVerb(phraseText);
-	let present = conjugateVerb(verb,tense);
+	let present = verb;
+
+	if (verb === undefined) {
+		console.warn(`Could not find a verb to conjugate in '${phraseText}'.`);
+	} else {
+		present = conjugateVerb(verb,tense);
+	}
 
 	phraseElement.querySelector('.phrase-container p').innerText = phraseText.replace(verb,present);
 }
-
-// TODO play a lookatme effect on diary page when library phrase picked up?
-// (if not right away, could trigger after some amount of time while picked up?)
-let librarySortableObject = {
-	animation: 200,
-	group: {
-		name: "library",
-		pull: "clone", // ability to copy from the list 
-		put: false, // no list can add to library
-		revertClone: true // revert cloned element to initial position after moving to a another list
-	},
-	//ghostClass: 'dragGhost',
-	dragClass: 'dragging',
-	sort: false,
-	filter: ".new-button",
-	// Element dragging started
-	onStart: function (evt) {
-		evt.item.classList.add('dragging');  // element index within parent
-	},
-	// TODO replace event listeners on original even if you don't finish dragging successfully
-	// Investigate what function fires when you drag a library phrase outside of library
-	// but not in diary
-
-	// Element dragging ended (dragged HTMLElement: evt.item)
-	onEnd: function (evt) {
-		// Remove event listeners from dragged element, put a new one on the clone
-		// (Because the clone is added to the library, the original element is dragged away)
-		//evt.item.removeEventListener('click',addToDiary);
-		//evt.clone.addEventListener('click',addToDiary);
-
-		updatePhraseListeners(evt.item, "diary");
-		updatePhraseListeners(evt.clone, "library");
-
-  		evt.to;    // target list
-  		evt.from;  // previous list
-  		evt.oldIndex;  // element's old index within old parent
-  		evt.newIndex;  // element's new index within new parent
-  		evt.oldDraggableIndex; // element's old index within old parent, only counting draggable elements
-  		evt.newDraggableIndex; // element's new index within new parent, only counting draggable elements
-  		evt.pullMode;  // when item is in another sortable: `"clone"` if cloning, `true` if moving
-	}
-};
-
-// Make each library category Sortable
-let libraries = Array.from(document.querySelectorAll('.library'));
-libraries.forEach( (library) => {
-	Sortable.create ( library, librarySortableObject);
-});
-
-Sortable.create( diary, {
-	animation: 200, 
-	group: {
-		name: "diary",
-		pull: ["trash", "inner"], // lists that can pull from diary 
-		put: ["library", "inner"], // lists that can add to diary
-		revertClone: true,
-	},
-	fallbackOnBody: true,
-	swapThreshold: 0.1,
-	invertSwap: true,
-	//ghostClass: 'dragGhost',
-	sort: true,
-	onAdd: function (evt) {
-		// When a phrase is dropped into the diary
-		addToDiary (evt.item);
-		undoPhraseTransforms(evt.item);
-	},
-	// Called by any change to the list (add / update / remove)
-	onSort: function (evt) {
-	},
-});
 
 // Add a given phrase/action/event (by id) to the diary entry
 // -- uses the event which triggered it (a phrase's onclick)
@@ -376,10 +280,7 @@ function addToDiary (newPhrase) {
 	}
 
 	// Make each nested inner slot Sortable
-	let innerSlots = Array.from(newPhrase.querySelectorAll('.inner-slot'));
-	innerSlots.forEach( (innerSlot) => {
-		Sortable.create(innerSlot, innerSortableObject);
-	});
+	makeInnerSlotsSortable(newPhrase);
 
 	// Add subject if phrase doesn't already have one
 	addSubject(newPhrase);
@@ -426,28 +327,6 @@ function hasSubject (phrase) {
 	
 	return false;
 }
-
-// A pseudo-list to enable drag-and-dropping from diary to trash
-// TODO trigger a trash effect when hovered over while dragging 
-// (Is there an onenter?)
-// And maybe a lookatme effect on trash when diary phrase is picked up
-// (e.g., trash lid opens)
-Sortable.create( trash, {
-	animation: 200,
-	group: {
-		name: "trash",
-		pull: false,
-		put: ["diary","inner"]
-	},
-	ghostClass: 'trashGhost', // Set display:none 
-	//dragClass: 'dragoverTrash',
-	sort: false,
-	filter: "#trash", // Selectors that do not lead to dragging
-	onAdd: function (evt) {
-		// When a phrase is dropped into the trash "list"
-		removeFromDiary(evt.item);
-	}
-});
 
 function removeFromDiary (phrase) {
 	phrase.remove();
---
+++ sortables.js
@@ -0,0 +1,151 @@
+// Functions and data that interface with the SortableJS library,
+// which enables drag-and-dropping elements to and from different lists on a page.
+
+// Make the inner slot Sortable definition (which is )
+// TODO since this doesn't allow arbitrarily nested inner slots,
+// might try making different Sortable group for each inner slot (by incrementing an id)?
+// and then connecting all existing inner slot groups
+// --Would also need to rework CSS probably
+// TODO ** Instead of inner sortable slots (since they make for small targets)
+// make it so when you drag and drop something over a phrase with a slot, 
+// it gets added to that slot (by hand; don't make inner slots Sortable lists).
+// if there are multiple slots in phrase, check mouse position relative to boundingRect
+let innerSortableObject = {
+	group: {
+		name: "inner",
+		pull: ["trash", "diary", "inner"], // lists that can pull from any inner slot 
+		put: ["library", "diary", "inner"], // lists that can add to any inner slot
+		revertClone: true,
+	},
+	animation: 200,
+	//fallbackOnBody: true,
+	sort: true,
+	// px, distance mouse must be from empty sortable to insert drag element into 
+	emptyInsertThreshold: 20,
+	filter: ".empty-indicator",  // Selectors that do not lead to dragging
+	onAdd: function (evt) {
+		// When a phrase is added to an inner slot, hide its empty-indicator
+		// and show a distinguishing background color
+		// Use :scope selector to select only direct children, not descendants
+		evt.to.querySelector(':scope > .empty-indicator').style.display = "none";
+		// TODO slightly darken the inner slot's parent color for the inner slot background 
+		evt.to.style.background = "#00a6de"; 
+		transformCompoundPhrase(evt.to.parentElement.parentElement, evt.item);
+	},
+	// Element is removed from the list into another list
+	onRemove: function (evt) {
+		// if list is empty, show empty indicator symbol
+		// TODO try doing this onChange so it's updated during the drag?
+		if (evt.from.children.length <= 1) {
+			evt.from.querySelector(':scope > .empty-indicator').style.display = "inline-block";
+			evt.from.style.background = "inherit";
+		}
+	}
+};
+
+// Make all of a given phrase element's inner slots Sortable 
+function makeInnerSlotsSortable (outerPhrase) {
+	// Make each nested inner slot Sortable
+	let innerSlots = Array.from(outerPhrase.querySelectorAll('.inner-slot'));
+	innerSlots.forEach( (innerSlot) => {
+		Sortable.create(innerSlot, innerSortableObject);
+	});
+}
+
+// TODO play a lookatme effect on diary page when library phrase picked up?
+// (if not right away, could trigger after some amount of time while picked up?)
+let librarySortableObject = {
+	animation: 200,
+	group: {
+		name: "library",
+		pull: "clone", // ability to copy from the list 
+		put: false, // no list can add to library
+		revertClone: true // revert cloned element to initial position after moving to a another list
+	},
+	//ghostClass: 'dragGhost',
+	dragClass: 'dragging',
+	sort: false,
+	filter: ".new-button",
+	// Element dragging started
+	onStart: function (evt) {
+		evt.item.classList.add('dragging');  // element index within parent
+	},
+	// TODO replace event listeners on original even if you don't finish dragging successfully
+	// Investigate what function fires when you drag a library phrase outside of library
+	// but not in diary
+
+	// Element dragging ended (dragged HTMLElement: evt.item)
+	onEnd: function (evt) {
+		// Remove event listeners from dragged element, put a new one on the clone
+		// (Because the clone is added to the library, the original element is dragged away)
+		//evt.item.removeEventListener('click',addToDiary);
+		//evt.clone.addEventListener('click',addToDiary);
+
+		updatePhraseListeners(evt.item, "diary");
+		updatePhraseListeners(evt.clone, "library");
+
+  		evt.to;    // target list
+  		evt.from;  // previous list
+  		evt.oldIndex;  // element's old index within old parent
+  		evt.newIndex;  // element's new index within new parent
+  		evt.oldDraggableIndex; // element's old index within old parent, only counting draggable elements
+  		evt.newDraggableIndex; // element's new index within new parent, only counting draggable elements
+  		evt.pullMode;  // when item is in another sortable: `"clone"` if cloning, `true` if moving
+	}
+};
+
+// Make each library category Sortable
+function makeLibrarySortables() {
+	let libraries = Array.from(document.querySelectorAll('.library'));
+	libraries.forEach( (library) => {
+		Sortable.create ( library, librarySortableObject);
+	});
+}
+
+// Make the diary entry Sortable
+Sortable.create( diary, {
+	animation: 200, 
+	group: {
+		name: "diary",
+		pull: ["trash", "inner"], // lists that can pull from diary 
+		put: ["library", "inner"], // lists that can add to diary
+		revertClone: true,
+	},
+	fallbackOnBody: true,
+	swapThreshold: 0.1,
+	invertSwap: true,
+	//ghostClass: 'dragGhost',
+	sort: true,
+	onAdd: function (evt) {
+		// When a phrase is dropped into the diary
+		addToDiary (evt.item);
+		undoPhraseTransforms(evt.item);
+	},
+	// Called by any change to the list (add / update / remove)
+	onSort: function (evt) {
+	},
+});
+
+// Make the trashcan Sortable
+// A pseudo-list to enable drag-and-dropping from diary to trash
+// TODO trigger a trash effect when hovered over while dragging 
+// (Is there an onenter?)
+// And maybe a lookatme effect on trash when diary phrase is picked up
+// (e.g., trash lid opens)
+Sortable.create( trash, {
+	animation: 200,
+	group: {
+		name: "trash",
+		pull: false,
+		put: ["diary","inner"]
+	},
+	ghostClass: 'trashGhost', // Set display:none 
+	//dragClass: 'dragoverTrash',
+	sort: false,
+	filter: "#trash", // Selectors that do not lead to dragging
+	onAdd: function (evt) {
+		// When a phrase is dropped into the trash "list"
+		removeFromDiary(evt.item);
+	}
+});
+
